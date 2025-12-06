238a57d7624713d75396977bd7557fa9d0a02913
AshleyThew <ashl.e.thew@gmail.com>
2025-12-07 10:28:59 +1300

    Add 'modifier' variable type to VariableEditor
    Introduces a new 'modifier' type for variables, allowing input of signed numbers with validation. Updates the setCharacterStat action in actions.json to use the 'modifier' type for the value variable.

--- src/components/editor/Variables.tsx
+++ src/components/editor/Variables.tsx
@@ -1,49 +1,66 @@
-import React from "react";
-import { DialogueContext } from "../DialogueContext";
-import { DropdownInput, EditableInput } from "./Inputs";
-import { createLabels } from "../../utils/Utils";
+import React from 'react';
+import { DialogueContext } from '../DialogueContext';
+import { DropdownInput, EditableInput } from './Inputs';
+import { createLabels } from '../../utils/Utils';
 
 export interface VariableProps {
-	source?: string;
-	type?: "text" | "number" | "list" | undefined;
-	placeholder?: string;
-	required?: boolean;
+  source?: string;
+  type?: 'text' | 'number' | 'list' | 'modifier' | undefined;
+  placeholder?: string;
+  required?: boolean;
 }
 
-export const VariableEditor = (props: { variable: VariableProps; setValue: Function; index: number; args: string[] }): JSX.Element => {
-	const { sources } = React.useContext(DialogueContext);
-	const { variable, setValue, index, args } = props;
-	var pattern = undefined;
-	var number = false;
-	switch (variable.type) {
-		case "number":
-			number = true;
-			pattern = /^[0-9]*$/;
-		// eslint-disable-next-line no-fallthrough
-		case "text": {
-			return (
-				<EditableInput
-					style={{ margin: "5px 2px", alignSelf: "flex-end" }}
-					value={args[index]}
-					setValue={setValue}
-					minLength={2}
-					placeholder={variable.placeholder}
-					pattern={pattern}
-					number={number}
-				/>
-			);
-		}
-		case "list": {
-			var source = variable.source;
-			var matches = /\[(.*?)\]/.exec(source);
-			if (matches) {
-				const number = Number(matches[1]);
-				source = source.replace(matches[1], args[index + number]);
-			}
-			return <DropdownInput values={createLabels(sources[source])} value={args[index]} setValue={setValue} placeholder={variable.placeholder} />;
-		}
-		default: {
-			return <div />;
-		}
-	}
+export const VariableEditor = (props: {
+  variable: VariableProps;
+  setValue: Function;
+  index: number;
+  args: string[];
+}): JSX.Element => {
+  const { sources } = React.useContext(DialogueContext);
+  const { variable, setValue, index, args } = props;
+  var pattern = undefined;
+  var number = false;
+  switch (variable.type) {
+    case 'number':
+      number = true;
+      pattern = /^[0-9]*$/;
+      break;
+    case 'modifier':
+      pattern = /^[+-]?[0-9]+$/;
+      number = true;
+      break;
+    // eslint-disable-next-line no-fallthrough
+    case 'text': {
+      return (
+        <EditableInput
+          style={{ margin: '5px 2px', alignSelf: 'flex-end' }}
+          value={args[index]}
+          setValue={setValue}
+          minLength={2}
+          placeholder={variable.placeholder}
+          pattern={pattern}
+          number={number}
+        />
+      );
+    }
+    case 'list': {
+      var source = variable.source;
+      var matches = /\[(.*?)\]/.exec(source);
+      if (matches) {
+        const number = Number(matches[1]);
+        source = source.replace(matches[1], args[index + number]);
+      }
+      return (
+        <DropdownInput
+          values={createLabels(sources[source])}
+          value={args[index]}
+          setValue={setValue}
+          placeholder={variable.placeholder}
+        />
+      );
+    }
+    default: {
+      return <div />;
+    }
+  }
 };
